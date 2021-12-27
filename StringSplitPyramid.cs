19b457017f48f2e851099d17e66e055c948fb92d
r-kls <93781769+r-kls@users.noreply.github.com>
2021-12-27 08:41:30 +0100

    Add files via upload

---
+++ StringSplitPyramid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringSplitPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadejte vstupní řetězec: ");
+            string retezec = Console.ReadLine();
+            string[] rozdeleny_retezec = retezec.Split(" ");
+            
+            for (int i = rozdeleny_retezec.Length; i > 0; i--)
+            {
+                for (int j  = 0; j < i; j++)
+                {
+                    Console.Write(rozdeleny_retezec[j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
