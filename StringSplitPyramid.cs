2bcf561218cad0ceeba78d191d687d7512ed54c6
r-kls <93781769+r-kls@users.noreply.github.com>
2021-12-27 08:54:27 +0100

    Add files via upload

--- StringSplitPyramid.cs
+++ StringSplitPyramid.cs
@@ -10,9 +10,9 @@ namespace StringSplitPyramid
             string retezec = Console.ReadLine();
             string[] rozdeleny_retezec = retezec.Split(" ");
             
-            for (int i = rozdeleny_retezec.Length; i > 0; i--)
+            for (int i = rozdeleny_retezec.Length; i > 0; i--) // Ubírá segment po každěm průběhu a odřádkovává již vypsané řetezce.
             {
-                for (int j  = 0; j < i; j++)
+                for (int j  = 0; j < i; j++) // Vypisuje jednotlivé segmenty a rozdělí je pokaždé mezerou.
                 {
                     Console.Write(rozdeleny_retezec[j]);
                     Console.Write(" ");
