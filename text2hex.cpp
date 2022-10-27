e98c38bc162f4a724cf6797976fae90e8285c041
rklss <93781769+rklss@users.noreply.github.com>
2022-10-27 09:39:26 +0000

    Add files via upload
    Added some comments and fixed a bug where the app printed wrong values if the char was not an ASCII char but stored in multiple bytes

--- text2hex.cpp
+++ text2hex.cpp
@@ -12,24 +12,43 @@ void convert_to_hex (unsigned int a) {
 		std::cout << hex_table[output_values[i]]; // All elements of the vector are printed to the user from the back
 	}
 }
-void text_conversion() {
+void print_values (unsigned char a) {
+	if (a >= 32 && a <= 126) {
+		std::cout << "\t" << a << "\n";
+	} else {
+		std::cout << "\t" << ".\n";
+	}
+}
+void text_conversion(bool hex_string_only) {
 	// Saving input text
 	std::string input_text;
 	std::cout << "Enter Text: ";
 	getline(std::cin, input_text);
+	if (hex_string_only) { // Prints only as a stream of hex chars
+		for (int i = 0; i < input_text.length(); ++i) {
+			convert_to_hex((unsigned int) (unsigned char) input_text[i]);
+		}
+		std::cout << "\n";
+		return;
+	} // Or else it prints in a "table"
 	// Table output
-	std::cout << "HEX\tNUM\n";
+	std::cout << "HEX\tNUM\tVAL\n";
 	for (int i = 0; i < input_text.length(); ++i) {
-		convert_to_hex(input_text[i]);
-		std::cout << "\t" << (unsigned int)input_text[i] << "\n";
+		convert_to_hex((unsigned int) (unsigned char) input_text[i]);
+		std::cout << "\t" << (unsigned int) (unsigned char) input_text[i];
+		print_values((unsigned char) input_text[i]);
 	}
 	std::cout << "\n";
 }
-void number_conversion() {
+void number_conversion(bool hex_string_only) {
 	// Saving input number
 	unsigned int input_number;
 	std::cout << "Enter number: ";
 	std::cin >> input_number;
+	if (hex_string_only) { // Same as in func text_conversion
+		convert_to_hex(input_number);
+		return;
+	}
 	// Table output
 	std::cout << "HEX\tNUM\n";
 	convert_to_hex(input_number);
@@ -38,15 +57,16 @@ void number_conversion() {
 }
 int main(int argc, char** argv) {
 	// Mode selection
+	bool hex_string_only = (argc == 3 && *argv[2] == 's' ? true : false); // Sets variable if 2nd argument was typed
 	if (argc == 1 || *argv[1] == 't') {
 		std::cout << "Text conversion!\n";
-		text_conversion();
+		text_conversion(hex_string_only);
 	} else if (*argv[1] == 'n') {
 		std::cout << "Number conversion!\n";
-		number_conversion();
+		number_conversion(hex_string_only);
 	} else if (*argv[1] == '?' || *argv[1] == 'h') {
 		std::cout << "Text to HEX converter - help menu\n";
-		std::cout << "Usage:\n\ttext2hex [args]\nArguments:\n\t t/none - Text -> HEX\n\t n - Number -> HEX\n\t ?/h - Show this help menu\n"; // Don't judge me
+		std::cout << "Usage:\n\ttext2hex [args]\nArguments:\n\t t/none - Text -> HEX\n\t n - Number -> HEX\n\t s ( 2nd argument ) - Prints only in hex as a stream\n\t?/h - Show this help menu\n"; // Don't judge me
 	} else {
 		std::cout << "Unrecognized argument! Exiting...\n";
 	}
